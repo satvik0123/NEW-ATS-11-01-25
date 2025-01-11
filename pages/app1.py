6f13a5ce4039c1a05ed8bbd79437bc13625b9df1
Satvik Gupta <satvikg21111124@gmial.com>
2025-01-12 00:09:17 +0530

    intialize commit

---
+++ pages/app1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Welcome to My Multi-Page Streamlit App")
+
+# You can add content here or direct users to different pages using buttons or links
+st.write("Navigate between pages using the sidebar on the left.")
